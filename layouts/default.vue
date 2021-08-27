972d3e8eea8614cf901fc3dd02741fd57f92cb57
MohamedZhran <mohamedzhran1@gmail.com>
2021-08-27 20:23:06 +0200

    init commit

---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app>
+    <v-main>
+      <v-container>
+        <Nuxt/>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  data()
+  {
+    return {}
+  }
+}
+</script>
+<style lang = "scss" scoped>
+
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div class = "text-right">
+      <NuxtLink to = "/cart">View Cart</NuxtLink>
+    </div>
+    <h1 class = "font-weight-bold text-center my-4">Products</h1>
+    <products-table/>
+  </div>
+</template>
+<script>
+export default {
+  data()
+  {
+    return {}
+  },
+}
+</script>
+<style lang = "scss">
+
+</style>
